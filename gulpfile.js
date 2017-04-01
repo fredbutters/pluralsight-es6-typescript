3757dbbd9069dca0fe18c7d96323a37e507fb442
MarkB45 <MarkB45@users.noreply.github.com>
2017-03-31 18:29:10 -0700

    add node server again

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     ts = require('gulp-typescript'),
     babel = require('gulp-babel'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload')
+    exec = require('child_process').exec;
 
-gulp.task('script', () => {  
+gulp.task('script', ['server'], () => {  
     return gulp.src('src/js/**/*.ts')
         .pipe(sourcemaps.init())
         .pipe(ts({
@@ -17,6 +18,12 @@ gulp.task('script', () => {
         .pipe(livereload());
 });
 
+gulp.task('server', (cb) => {
+    exec('node server.js', (err, stdout, stderr) => {
+        console.log(err);
+        cb(err);
+    });
+});
 gulp.task('default', () => {
     livereload.listen();
     gulp.watch('src/js/**/*.ts', ['script']);
